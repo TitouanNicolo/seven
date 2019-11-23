716c5e168a85da4da2a99097c38b89f20aa2ecb2
Damien <damien.fauveau@outlook.com>
2019-11-23 12:16:38 +0100

    First commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,236 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Controller2D))]
+public class Player : MonoBehaviour {
+
+	public float maxJumpHeight = 4;
+	public float minJumpHeight = 1;
+	public float timeToJumpApex = .4f;
+	float accelerationTimeAirborne = .2f;
+	float accelerationTimeGrounded = .1f;
+	float moveSpeed = 6;
+
+	public Vector2 wallJumpClimb;
+	public Vector2 wallJumpOff;
+	public Vector2 wallLeap;
+
+	public float wallSlideSpeedMax = 3;
+	public float wallStickTime = .25f;
+	float timeToWallUnstick;
+
+	float gravity;
+	float maxJumpVelocity;
+	float minJumpVelocity;
+	Vector3 velocity;
+	float velocityXSmoothing;
+
+	Controller2D controller;
+
+	Vector2 directionalInput;
+	bool wallSliding;
+	int wallDirX;
+
+	[SerializeField] float      m_speed = 1.0f;
+    [SerializeField] float      m_jumpForce = 2.0f;
+
+    private Animator            m_animator;
+    private Rigidbody2D         m_body2d;
+    private Sensor_Bandit       m_groundSensor;
+    private bool                m_grounded = false;
+    private bool                m_combatIdle = false;
+    private bool                m_isDead = false;
+
+    private int m_item = 0;
+    private float timeLeft = 300;
+
+	void Start() {
+		controller = GetComponent<Controller2D> ();
+
+		gravity = -(2 * maxJumpHeight) / Mathf.Pow (timeToJumpApex, 2);
+		maxJumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+		minJumpVelocity = Mathf.Sqrt (2 * Mathf.Abs (gravity) * minJumpHeight);
+
+		m_animator = GetComponent<Animator>();
+        m_body2d = GetComponent<Rigidbody2D>();
+        m_groundSensor = transform.Find("GroundSensor").GetComponent<Sensor_Bandit>();
+	}
+
+	void Update() {
+		CalculateVelocity ();
+		HandleWallSliding ();
+
+		controller.Move (velocity * Time.deltaTime, directionalInput);
+
+		if (controller.collisions.above || controller.collisions.below) {
+			if (controller.collisions.slidingDownMaxSlope) {
+				velocity.y += controller.collisions.slopeNormal.y * -gravity * Time.deltaTime;
+			} else {
+				velocity.y = 0;
+			}
+		}
+
+		//Check if character just landed on the ground
+        if (!m_grounded && m_groundSensor.State()) {
+            m_grounded = true;
+            m_animator.SetBool("Grounded", m_grounded);
+        }
+
+        //Check if character just started falling
+        if(m_grounded && !m_groundSensor.State()) {
+            m_grounded = false;
+            m_animator.SetBool("Grounded", m_grounded);
+        }
+
+        // -- Handle input and movement --
+        float inputX = Input.GetAxis("Horizontal");
+
+        // Swap direction of sprite depending on walk direction
+        if (inputX > 0)
+            transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+        else if (inputX < 0)
+            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+
+        // Move
+        m_body2d.velocity = new Vector2(inputX * m_speed, m_body2d.velocity.y);
+
+        //Set AirSpeed in animator
+        m_animator.SetFloat("AirSpeed", m_body2d.velocity.y);
+
+        // -- Handle Animations --
+        //Death
+        if (Input.GetKeyDown("e")) {
+            if(!m_isDead)
+                m_animator.SetTrigger("Death");
+            else
+                m_animator.SetTrigger("Recover");
+
+            m_isDead = !m_isDead;
+        }
+            
+        //Hurt
+        else if (Input.GetKeyDown("q"))
+            m_animator.SetTrigger("Hurt");
+
+        //Attack
+        else if(Input.GetMouseButtonDown(0)) {
+            m_animator.SetTrigger("Attack");
+        }
+
+        //Change between idle and combat idle
+        else if (Input.GetKeyDown("f"))
+            m_combatIdle = !m_combatIdle;
+
+        //Jump
+        else if (Input.GetKeyDown("space") && m_grounded) {
+            m_animator.SetTrigger("Jump");
+            m_grounded = false;
+            m_animator.SetBool("Grounded", m_grounded);
+            m_body2d.velocity = new Vector2(m_body2d.velocity.x, m_jumpForce);
+            m_groundSensor.Disable(0.2f);
+        }
+
+        //Run
+        else if (Mathf.Abs(inputX) > Mathf.Epsilon)
+            m_animator.SetInteger("AnimState", 2);
+
+        //Combat Idle
+        else if (m_combatIdle)
+            m_animator.SetInteger("AnimState", 1);
+
+        //Idle
+        else
+            m_animator.SetInteger("AnimState", 0);
+
+		timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            m_animator.SetTrigger("Death");
+        }
+	}
+
+	public void SetDirectionalInput (Vector2 input) {
+		directionalInput = input;
+	}
+
+	public void OnJumpInputDown() {
+		if (wallSliding) {
+			if (wallDirX == directionalInput.x) {
+				velocity.x = -wallDirX * wallJumpClimb.x;
+				velocity.y = wallJumpClimb.y;
+			}
+			else if (directionalInput.x == 0) {
+				velocity.x = -wallDirX * wallJumpOff.x;
+				velocity.y = wallJumpOff.y;
+			}
+			else {
+				velocity.x = -wallDirX * wallLeap.x;
+				velocity.y = wallLeap.y;
+			}
+		}
+		if (controller.collisions.below) {
+			if (controller.collisions.slidingDownMaxSlope) {
+				if (directionalInput.x != -Mathf.Sign (controller.collisions.slopeNormal.x)) { // not jumping against max slope
+					velocity.y = maxJumpVelocity * controller.collisions.slopeNormal.y;
+					velocity.x = maxJumpVelocity * controller.collisions.slopeNormal.x;
+				}
+			} else {
+				velocity.y = maxJumpVelocity;
+			}
+		}
+	}
+
+	public void OnJumpInputUp() {
+		if (velocity.y > minJumpVelocity) {
+			velocity.y = minJumpVelocity;
+		}
+	}
+		
+
+	void HandleWallSliding() {
+		wallDirX = (controller.collisions.left) ? -1 : 1;
+		wallSliding = false;
+		if ((controller.collisions.left || controller.collisions.right) && !controller.collisions.below && velocity.y < 0) {
+			wallSliding = true;
+
+			if (velocity.y < -wallSlideSpeedMax) {
+				velocity.y = -wallSlideSpeedMax;
+			}
+
+			if (timeToWallUnstick > 0) {
+				velocityXSmoothing = 0;
+				velocity.x = 0;
+
+				if (directionalInput.x != wallDirX && directionalInput.x != 0) {
+					timeToWallUnstick -= Time.deltaTime;
+				}
+				else {
+					timeToWallUnstick = wallStickTime;
+				}
+			}
+			else {
+				timeToWallUnstick = wallStickTime;
+			}
+
+		}
+
+	}
+
+	void CalculateVelocity() {
+		float targetVelocityX = directionalInput.x * moveSpeed;
+		velocity.x = Mathf.SmoothDamp (velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below)?accelerationTimeGrounded:accelerationTimeAirborne);
+		velocity.y += gravity * Time.deltaTime;
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Check for a match with the specific tag on any GameObject that collides with your GameObject
+        if (collision.gameObject.tag == "Item")
+        {
+            Destroy(collision.gameObject);
+            m_item++;
+            
+            Debug.Log($"Nombre item : {m_item}" );
+        }
+    }
+}
