2237fcc5e0052df834a41f0c0890f450a01b12a6
Damien <damien.fauveau@outlook.com>
2019-11-23 12:46:33 +0100

    Fixed collision right

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -87,9 +87,9 @@ public class Player : MonoBehaviour {
 
         // Swap direction of sprite depending on walk direction
         if (inputX > 0)
-            transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+            transform.localScale = new Vector3(-2.0f, 2.0f, 2.0f);
         else if (inputX < 0)
-            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+            transform.localScale = new Vector3(2.0f, 2.0f, 2.0f);
 
         // Move
         m_body2d.velocity = new Vector2(inputX * m_speed, m_body2d.velocity.y);
